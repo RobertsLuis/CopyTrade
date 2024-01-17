fef369257157ad5717ecb61d5c7c9b5a359ac55a
Roberto Luis <130838869+RobertsLuis@users.noreply.github.com>
2024-01-17 03:07:26 -0300

    Normalizing git integration

--- main.py
+++ main.py
@@ -814,7 +814,7 @@ if __name__ == '__main__':
         tradeEvent = Event()
 
         startTime = __getCurrentTime()
-        print("Starting... with Victor now 1")
+        print("Starting...")
         num_cores = os.cpu_count()
         #print(f"Número de núcleos da CPU: {num_cores}")
         api = IQ_Option('winnerzonebot@gmail.com', 'WinnerzoneBOT1!')
